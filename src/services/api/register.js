2912e9d9a92161a8e5732c6128b33281dd285769
t_aga <aga.lhj@link8tech.tw>
2021-09-01 17:42:33 +0800

    移除console

--- src/services/api/register.js
+++ src/services/api/register.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export const register = async (username, password) => {
-    console.log('username', username);
-    console.log('password', password);
-    const response = await axios.post('/api/register', { username, password });
-    console.log('response', response);
+    await axios.post('/api/register', { username, password });
 };
