c56219c95d29aa455bccad23fd9eda48cd2c6e3f
funfish22 <j26391442@gmail.com>
2021-09-05 23:12:30 +0800

    新增 登入驗證(未完成)

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
 
+import { authentication } from '@api/login';
+
 const AppPage = () => {
+    useEffect(() => {
+        let token = JSON.parse(localStorage.getItem('__mock_token__'));
+        const res = authentication(token);
+        console.log('res', res);
+        res.then((response) => {
+            console.log('response', response);
+        }).catch((err) => {
+            console.log('err.response.status', err);
+        });
+    });
     return (
         <BrowserRouter>
             <Router />
--- src/services/api/login.js
+++ src/services/api/login.js
@@ -3,3 +3,18 @@ import axios from 'axios';
 export const login = async (username, password) => {
     await axios.post('/api/login', { username, password });
 };
+
+export const authentication = async (token) => {
+    await axios
+        .get('/api/authentication', {
+            headers: {
+                AUTHENTICATION_TOKEN: token,
+            },
+        })
+        .then((response) => {
+            return response;
+        })
+        .catch((err) => {
+            return Promise.reject(err);
+        });
+};
